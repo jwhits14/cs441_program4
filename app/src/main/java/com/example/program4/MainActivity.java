67859d4018ae5bcf43e3ce2023177d26b274d434
jwhits14 <jakewhit14@gmail.com>
2021-03-12 11:38:20 -0500

    finished app

--- app/src/main/java/com/example/program4/MainActivity.java
+++ app/src/main/java/com/example/program4/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -32,20 +34,23 @@ public class MainActivity extends AppCompatActivity {
         text.setText(user_input.getText());
         tableRow.addView(text, 0);
 
-        TextView text2 = new TextView(this);
-        text2.setText("new text view 2 | " + tableRowList.size());
-        tableRow.addView(text2, 1);
+        CheckBox check = new CheckBox(this);
+        tableRow.addView(check, 1);
+
+        Button button = new Button(this);
+        button.setText("Remove");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                table.removeView(tableRow);
+                tableRowList.remove(tableRow);
+            }
+        });
+        tableRow.addView(button, 2);
 
         table.addView(tableRow);
 
         tableRowList.add(tableRow);
 
-    }
-
-    public void removeRow(View view) {
-        if(tableRowList.size() > 0) {
-            TableRow row = tableRowList.remove(tableRowList.size() - 1);
-            table.removeView(row);
-        }
     }
 }
